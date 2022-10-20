0e5274ed629349e696cd15f63e00c872650639c7
Mavis Daffa Wisanggeni <mavisdaffawisanggeni22@gmail.com>
2022-10-20 14:27:18 +0700

    style: [MW] add utils

---
+++ lib/utils/constant.dart
@@ -0,0 +1,13 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+
+Color primary = Color(0xffAC7762);
+
+class Styles {
+  static Color primaryColor = primary;
+  static Color lightBrown = const Color(0xffECD5B6);
+  static Color pink = const Color(0xffE5989B);
+  static Color grey = const Color(0xff9A9A9A);
+  static Color black = Colors.black;
+  static Color white = Colors.white;
+}
